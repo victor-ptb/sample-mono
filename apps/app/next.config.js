a4331b2fb423dccf72561c69ec6e5c5098066047
victor-martinez-ptblink <victor.martinez@ptblink.com>
2022-11-05 10:41:37 +1100

    test 2

--- apps/app/next.config.js
+++ apps/app/next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const CopyPlugin = require("copy-webpack-plugin");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
