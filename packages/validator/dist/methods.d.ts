98b53a38a5eefe053b6df5703a6a2f0016f53958
victor-martinez-ptblink <victor.martinez@ptblink.com>
2022-11-05 10:08:16 +1100

    first

---
+++ packages/validator/dist/methods.d.ts
@@ -0,0 +1,6 @@
+/**
+ * Checks if given value is null or undefined or whitespace string
+ * @param {string?} value
+ */
+export declare const isNullOrWhitespace: (value?: string | null) => boolean;
+//# sourceMappingURL=methods.d.ts.map
